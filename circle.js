580b69aa97a99a1f6de38649e012793d44e8472e
Gary 170 <78164004+Gary170@users.noreply.github.com>
2023-02-20 13:32:34 +0530

    Correct

--- circle.js
+++ circle.js
@@ -1,2 +1,10 @@
 function calculateArea() {
-  var radius = document.getElementById("radius").value;
+  // Get the value of the radius input field
+  const radius = document.getElementById("radius").value;
+
+  // Calculate the area of the circle
+  const area = Math.PI * radius ** 2;
+
+  // Display the area of the circle
+  document.getElementById("area").textContent = area.toFixed(2);
+}
