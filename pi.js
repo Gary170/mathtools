77c052998b5efed2a0e71b01b03f72f4d3b9a49e
Gary 170 <78164004+Gary170@users.noreply.github.com>
2023-02-20 13:41:10 +0530

    I

--- pi.js
+++ pi.js
@@ -1,1 +1,10 @@
-document.getElementById("pi").textContent = Math.PI;
+function displayPi() {
+  // Get the number of decimal places to display
+  const decimalPlaces = document.getElementById("decimal-places").value;
+
+  // Calculate the value of pi to the specified number of decimal places
+  const pi = Math.PI.toFixed(decimalPlaces);
+
+  // Display the value of pi
+  document.getElementById("pi").textContent = pi;
+}
